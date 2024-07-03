b96bf558c35bcd1a1d49c922bea19de8e75d30a4
rutujapol <rutujapol20@gmail.com>
2024-07-03 10:49:13 +0530

    Add sum_of_array.cpp to Array directory

---
+++ DSA/Array/sum_of_array.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int arr[6]={1,2,3,4,5,6};
+    int sum = 0;
+    for(int i=0;i<6;i++){
+        sum += arr[i];
+    }
+    cout<<sum<<endl;
+    
+
+return 0;
+}
